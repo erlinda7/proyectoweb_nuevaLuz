0c1af2075be7c16f1566ec48c11ed99edfa204a5
Ariel Patzi <a_px2_pegaso@hotmail.com>
2020-01-24 09:51:07 -0400

    diseño Inicio

--- nuevaluz_frontend/src/components/inicioCarrusel.vue
+++ nuevaluz_frontend/src/components/inicioCarrusel.vue
@@ -3,13 +3,13 @@
     <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active" data-interval="10000">
-          <img src="@/assets/Carrusel_Inicio1.jpeg" class="d-block w-100" alt="..." height="500px"/>
+          <img id="bordeCir" src="@/assets/Carrusel_Inicio1.jpeg" class="d-block w-100" alt="..." height="500px"/>
         </div>
         <div class="carousel-item" data-interval="2000">
-          <img src="@/assets/Carrusel_Inicio2.jpeg" class="d-block w-100" alt="..." height="500px"/>
+          <img id="bordeCir" src="@/assets/Carrusel_Inicio2.jpeg" class="d-block w-100" alt="..." height="500px"/>
         </div>
         <div class="carousel-item">
-          <img src="@/assets/Carrusel_Inicio3.jpeg" class="d-block w-100" alt="..." height="500px"/>
+          <img id="bordeCir" src="@/assets/Carrusel_Inicio3.jpeg" class="d-block w-100" alt="..." height="500px"/>
         </div>
       </div>
       <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
@@ -29,4 +29,7 @@ export default {};
 </script>
 
 <style>
+#bordeCir{
+  border-radius: 20px
+}
 </style>
