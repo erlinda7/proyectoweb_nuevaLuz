06ddb1659959efd80459d2188b1e6f0dd2710cbf
Ariel Patzi <a_px2_pegaso@hotmail.com>
2020-01-25 19:35:43 -0400

    funcionando rutas de los ministerios

--- nuevaluz_frontend/src/views/Ministerio_Descripcion.vue
+++ nuevaluz_frontend/src/views/Ministerio_Descripcion.vue
@@ -1,19 +1,30 @@
 <template>
-  <div>
-    <p>vista Femenil---///Aqui se implementara todas vitas de descrpcion ministerios por id</p>
-    
-    <div v-for="(ministerio,index) in filtrarMinisterioId()" :key="index">
-      <p>id_ministerio:{{ministerio.id_ministerio}}</p>
-      <p>Titulo:{{ministerio.nombre}}</p>
-      <p>descripcion:{{ministerio.descripcion}}</p>
-      <p>lugar:{{ministerio.lugar}}</p>
-      <p>nombre_responsable:{{ministerio.nombre_responsable}}</p>
-      <p>email:{{ministerio.email}}</p>
-      <p>telefono:{{ministerio.telefono}}</p>
-      <img :src="require('@/fotos/' + ministerio.foto)" alt="imagen" width="20%" />
-      <br />
-      <br />
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        {{$route.params.id}}
+        falta terminar ;)
+      </div>
+      <div class="col-sm-6">
+
+      </div>
     </div>
+    
+    <div v-for="(ministerio,index) in ministerios" :key="index">
+      <div v-if="ministerio.id_ministerio==$route.params.id">
+        <p>id_ministerio:{{ministerio.id_ministerio}}</p>
+        <p>Titulo:{{ministerio.nombre}}</p>
+        <p>descripcion:{{ministerio.descripcion}}</p>
+        <p>lugar:{{ministerio.lugar}}</p>
+        <p>nombre_responsable:{{ministerio.nombre_responsable}}</p>
+        <p>email:{{ministerio.email}}</p>
+        <p>telefono:{{ministerio.telefono}}</p>
+        <img :src="require('@/fotos/' + ministerio.foto)" alt="imagen" width="20%" />
+        <br />
+        <br />
+      </div>
+    </div>
+    
   </div>
 </template>
 
@@ -34,15 +45,14 @@ export default {
         this.ministerios = respuesta.data;
       } catch (error) {
         console.log("error al conectar al api: ", error);
+        console.log(this.ministerios);
       }
     },
-    obtenerId() {
-      this.id = 1;   //implementar parmas.id
-      return this.id;
-    },
+    //no uso este metodo para filtrar, por que al recargar(F5) se pierde todo
+    //estoy usando v-if
     filtrarMinisterioId() {
       return (this.ministerioSeleccionado = this.ministerios.filter(
-        ministerio => ministerio.id_ministerio === this.obtenerId()
+        ministerio => ministerio.id_ministerio === this.$route.params.id
       ));
     }
   }
--- nuevaluz_frontend/src/views/Ministerios.vue
+++ nuevaluz_frontend/src/views/Ministerios.vue
@@ -1,67 +1,25 @@
 <template>
   <div class="container">
     <div class="row row-cols-1 row-cols-md-2">
-      <div class="col mb-4">
-        <div class="card " style="max-width: 20rem; margin:auto">
-         <img src="@/assets/fotoiglesia.jpg" class="card-img-top" alt="...">
-         <div class="card-img-overlay ">
-           <h5 class="card-title circulo">Evangelistassdasdsasdasksks</h5>
-         </div>
-         <div class="card-body">
-           <h5 class="card-title">Card title</h5>
-           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-         </div>
-        </div>
-      </div>
-      <div class="col mb-4">
-        <div class="card">
-          <img src="" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div  v-for="(ministerio,index) in ministerios" :key="index">
+        <div class="col mb-4">
+          <div class="card " style="max-width: 20rem; margin:auto">
+            <router-link :to="{name:'Ministerio_Descripcion', params:{id:ministerio.id_ministerio}}">
+              <img :src="require('@/assets/' + ministerio.imagen)" class="card-img-top" alt="imagen" />
+              <div class="card-img-overlay ">
+                <h5 class="card-title circulo">{{ministerio.nombre}}</h5>
+              </div>
+              <div class="card-body">
+                <h5>{{ministerio.nombre}}</h5>
+                <p class="card-text">:{{ministerio.descripcion_corta}}</p>
+              </div>
+            </router-link>
           </div>
         </div>
       </div>
-      <div class="col mb-4">
-        <div class="card">
-          <img src="" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col mb-4">
-       <div class="card">
-         <img src="" class="card-img-top" alt="...">
-        <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-       </div>
-      </div>
+      <router-view/>
   </div>
-    <p>vista ministerios</p>
-    <router-link to="/Ministerio_Descripcion">Ministerio Descripcion</router-link>
-    <br />
-    <router-link to="/Ministerio_Juvenil">Juvenil</router-link>
-    <br />
-    <router-link to="/Ministerio_Prejuvenil">Prejuvenil</router-link>
-    <br />
-    <router-link to="/Ministerio_Misioneros">Misioneros</router-link>
-    <br />
-    <router-link to="/Ministerio_Evangelistas">Evangelistas</router-link>
-    <br />
-    <router-link to="/Ministerio_Tesoritos">Tesoritos</router-link>
-    <br />
-    <router-link to="/Ministerio_Joyitas">Joyitas</router-link>
-    <br />
-    <router-link to="/Ministerio_HoritaFeliz">HoritaFeliz</router-link>
-    <br />
-    <router-link to="/Ministerio_Oanza">Oanza</router-link>
-
-    <br />
-    <br />
+    <!--
     <div v-for="(ministerio,index) in ministerios" :key="index">
         <p>id_ministerio:{{ministerio.id_ministerio}}</p>
         <p>Titulo:{{ministerio.nombre}}</p>
@@ -69,6 +27,7 @@
          <img :src="require('@/assets/' + ministerio.imagen)" alt="imagen" width="50%" />
         <br><br>
     </div>
+    -->
   </div>
 </template>
 
@@ -99,15 +58,15 @@ export default {
   font-family: 'Times New Roman', Times, serif;
   color: white;
   margin: auto;
-  width: 200px;
-  height: 200px;
+  width: 180px;
+  height: 180px;
   -moz-border-radius: 50%;
   -webkit-border-radius: 50%;
   border-radius: 50%;
   background: green;
   opacity: 0.8;
   filter: alpha(opacity=50);
-  padding-top: 75px;
+  padding-top: 50px;
   font-size: 30px;
   font-weight: bold;
   
