4eb53fc5d5b4f32cd84209734682d3335551a47e
Ariel Patzi <a_px2_pegaso@hotmail.com>
2020-01-25 16:17:01 -0400

    ministerio diseño

--- nuevaluz_frontend/src/views/Ministerios.vue
+++ nuevaluz_frontend/src/views/Ministerios.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="contanier">
+  <div class="container">
+    <div class="row row-cols-1 row-cols-md-2">
+      <div class="col mb-4">
+        <div class="card " style="max-width: 20rem; margin:auto">
+         <img src="@/assets/fotoiglesia.jpg" class="card-img-top" alt="...">
+         <div class="card-img-overlay ">
+           <h5 class="card-title circulo">Evangelistassdasdsasdasksks</h5>
+         </div>
+         <div class="card-body">
+           <h5 class="card-title">Card title</h5>
+           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+         </div>
+        </div>
+      </div>
+      <div class="col mb-4">
+        <div class="card">
+          <img src="" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-4">
+        <div class="card">
+          <img src="" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-4">
+       <div class="card">
+         <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+       </div>
+      </div>
+  </div>
     <p>vista ministerios</p>
     <router-link to="/Ministerio_Descripcion">Ministerio Descripcion</router-link>
     <br />
@@ -54,4 +95,26 @@ export default {
 </script>
 
 <style>
+.circulo {
+  font-family: 'Times New Roman', Times, serif;
+  color: white;
+  margin: auto;
+  width: 200px;
+  height: 200px;
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+  background: green;
+  opacity: 0.8;
+  filter: alpha(opacity=50);
+  padding-top: 75px;
+  font-size: 30px;
+  font-weight: bold;
+  
+}
+.centrarTodo{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap
+}
 </style>
