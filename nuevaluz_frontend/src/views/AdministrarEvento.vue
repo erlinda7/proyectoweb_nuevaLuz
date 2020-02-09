114bf253a1294bc2f3b5f2f9f2775c5cac9c344e
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-02-09 18:29:34 -0400

    limpiando formulario crear evento despues de ser creado

--- nuevaluz_frontend/src/views/AdministrarEvento.vue
+++ nuevaluz_frontend/src/views/AdministrarEvento.vue
@@ -1,102 +1,147 @@
 <template>
   <div class="container">
-      <br>
-      <div class="row">
-        <div  class="col-sm">
-          <router-link to="/Login"><button style="float: right" class="btn btn-danger" v-on:click="cerrarSesion()">Cerrar Sesion</button></router-link>
-          <router-link to="/Administrar"><button style="float: right" class="btn btn-primary">Atras</button></router-link>
-        </div>
+    <br />
+    <div class="row">
+      <div class="col-sm">
+        <router-link to="/Login">
+          <button
+            style="float: right"
+            class="btn btn-danger"
+            v-on:click="cerrarSesion()"
+          >Cerrar Sesion</button>
+        </router-link>
+        <router-link to="/Administrar">
+          <button style="float: right" class="btn btn-primary">Atras</button>
+        </router-link>
       </div>
-      <br>
+    </div>
+    <br />
     <!--FORMULARIO PARA CREAR EVENTO-->
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <b-form @submit="onSubmit" v-if="show">
+        <b-form @submit="onSubmit" v-if="show" >
           <h1>Registrar Evento</h1>
-          <b-form-group id="input-group-3" label="Potada del evento" label-for="input-3">
-            <b-form-file  v-model="file" :state="Boolean(file)" placeholder="Ingresa Imagen" drop-placeholder="Drop file here...">
-            </b-form-file>
+          <br><br>
+          <b-form-group id="input-group-3" label="Portada del evento" label-for="input-3" >
+            <b-form-file
+              v-model="file"
+              :state="Boolean(file)"
+              placeholder="Selecciona Imagen"
+              drop-placeholder="Drop file here..."
+            ></b-form-file>
           </b-form-group>
 
-          <b-form-group id="input-group-3" label-for="input-3">
+          <b-form-group id="input-group-3" label-for="input-3" description="Debe seleccionar el afiche del aevento">
             <b-form inline>
               <b-input v-model="form.imagen" disabled></b-input>
               <b-button variant="primary" v-on:click="enviarImagen()">Guardar Imagen</b-button>
             </b-form>
           </b-form-group>
-      
+            <br>
           <b-form-group id="input-group-1" label="Nombre del evento:" label-for="input-1">
-            <b-form-input id="input-1" v-model="form.nombre" required placeholder="Ingresa un nombre">
-            </b-form-input>
+            <b-form-input
+              id="input-1"
+              v-model="form.nombre"
+              required
+              placeholder="Ingresa un nombre"
+            ></b-form-input>
           </b-form-group>
-
+          <br>
           <b-form-group label="Descripcion del evento" label-for="textarea-formatter">
-            <b-form-textarea id="textarea-formatter" v-model="form.descripcion" placeholder="Ingresa la descripcion" required>
-            </b-form-textarea>
+            <b-form-textarea
+              id="textarea-formatter"
+              v-model="form.descripcion"
+              placeholder="Ingresa la descripcion"
+              required
+            ></b-form-textarea>
           </b-form-group>
-
+          <br>
           <b-form-group id="input-group-2" label="Lugar del evento" label-for="input-2">
-            <b-form-input id="input-2" v-model="form.lugar" required placeholder="Ingresa el lugar">
-            </b-form-input>
+            <b-form-input id="input-2" v-model="form.lugar" required placeholder="Ingresa el lugar"></b-form-input>
           </b-form-group>
-    
+          <br>
           <div class="form-group">
-            <label >Fecha del evento</label>
-            <input v-model="form.fecha" type="date" name="bday" min="1000-01-01" max="3000-12-31" class="form-control">
+            <label>Fecha del evento</label>
+            <input
+              v-model="form.fecha"
+              type="date"
+              name="bday"
+              min="1000-01-01"
+              max="3000-12-31"
+              class="form-control"
+            />
           </div>
-
-            <b-button type="submit" variant="primary">Submit</b-button>
+            <br>
+          <b-button type="submit" variant="primary">Crear Evento</b-button>
         </b-form>
-        
+        <br><br>
       </div>
     </div>
     <div>
       <!--FORMULARIO DE MODIFICACION DE EVENTO-->
       <div class="row justify-content-center">
-      <div class="col-sm-6">
-        <b-form @submit="onUpdate" v-if="!show">
+        <div class="col-sm-6">
+          <b-form @submit="onUpdate" v-if="!show">
             <h1>Actualizar Evento</h1>
-          <b-form-group id="input-group-3" label="Potada del evento" label-for="input-3">
-            <b-form-file  v-model="file" :state="Boolean(file)" placeholder="Ingresa Imagen" drop-placeholder="Drop file here...">
-            </b-form-file>
-          </b-form-group>
-
-          <b-form-group id="input-group-3" label-for="input-3">
-            <b-form inline>
-              <b-input v-model="evento.imagen" disabled></b-input>
-              <b-button variant="primary" v-on:click="actualizarImagen()">Guardar Imagen</b-button>
-            </b-form>
-          </b-form-group>
-      
-          <b-form-group id="input-group-1" label="Nombre del evento:" label-for="input-1">
-            <b-form-input id="input-1" v-model="evento.titulo" required placeholder="Ingresa un nombre">
-            </b-form-input>
-          </b-form-group>
-
-          <b-form-group label="Descripcion del evento" label-for="textarea-formatter">
-            <b-form-textarea id="textarea-formatter" v-model="evento.descripcion" placeholder="Ingresa la descripcion" required>
-            </b-form-textarea>
-          </b-form-group>
-
-          <b-form-group id="input-group-2" label="Lugar del evento" label-for="input-2">
-            <b-form-input id="input-2" v-model="evento.lugar" required placeholder="Ingresa el lugar">
-            </b-form-input>
-          </b-form-group>
-    
-          <div class="form-group">
-            <label >Fecha del evento</label>
-            <input v-model="evento.fecha" min="1000-01-01" max="3000-12-31" class="form-control">
-          </div>
-  
-          <!-- <div class="mt-3">Selecciona una opcion: {{ file ? file.name : '' }}</div> -->
-            <b-button type="update" variant="primary">Actualizar</b-button>
-        </b-form>
-        <!-- <b-card class="mt-3" header="Form Data Result">
+            <br><br>
+            <b-form-group id="input-group-3" label="Portada del evento" label-for="input-3">
+              <b-form-file
+                v-model="file"
+                :state="Boolean(file)"
+                placeholder="Selecciona  Imagen"
+                drop-placeholder="Drop file here..."
+              ></b-form-file>
+            </b-form-group>
+            
+            <b-form-group id="input-group-3" label-for="input-3" description="Debe seleccionar el afiche del evento">
+              <b-form inline>
+                <b-input v-model="evento.imagen" disabled></b-input>
+                <b-button variant="primary" v-on:click="actualizarImagen()">Guardar Imagen</b-button>
+              </b-form>
+            </b-form-group>
+            <br>
+            <b-form-group id="input-group-1" label="Nombre del evento:" label-for="input-1">
+              <b-form-input
+                id="input-1"
+                v-model="evento.titulo"
+                required
+                placeholder="Ingresa un nombre"
+              ></b-form-input>
+            </b-form-group>
+              <br>
+            <b-form-group label="Descripcion del evento" label-for="textarea-formatter">
+              <b-form-textarea
+                id="textarea-formatter"
+                v-model="evento.descripcion"
+                placeholder="Ingresa la descripcion"
+                required
+              ></b-form-textarea>
+            </b-form-group>
+              <br>
+            <b-form-group id="input-group-2" label="Lugar del evento" label-for="input-2">
+              <b-form-input
+                id="input-2"
+                v-model="evento.lugar"
+                required
+                placeholder="Ingresa el lugar"
+              ></b-form-input>
+            </b-form-group>
+              <br>
+            <div class="form-group">
+              <label>Fecha del evento</label>
+              <input v-model="evento.fecha" min="1000-01-01" max="3000-12-31" class="form-control" />
+            </div>
+            <br>
+            <!-- <div class="mt-3">Selecciona una opcion: {{ file ? file.name : '' }}</div> -->
+            <b-button type="update" variant="primary">Actualizar Evento</b-button>
+          </b-form>
+          <br><br>
+          <!-- <b-card class="mt-3" header="Form Data Result">
           <pre class="m-0">{{ evento }}</pre>
-        </b-card> -->
+          </b-card>-->
+        </div>
       </div>
-    </div>
-    <!--LISTA DE EVENTOS-->
+      <!--LISTA DE EVENTOS-->
     </div>
     <h1>Lista de Eventos</h1>
     <div>
@@ -111,16 +156,23 @@
         <tbody>
           <tr v-for="(evento,index) of eventos" :key="index">
             <th scope="row">{{evento.titulo}}</th>
-            <td><button class="btn btn-warning" v-on:click="cargarDatos(evento)" >Modificar</button></td>
+            <td>
+              <button class="btn btn-warning" v-on:click="cargarDatos(evento)">Modificar</button>
+            </td>
             <td>
               <b-button variant="danger" id="show-btn" @click="showModal(index)">Eliminar</b-button>
-                <b-modal :ref="'modal_'+index" hide-footer title="Seguro de Eliminar el Evento:">
+              <b-modal :ref="'modal_'+index" hide-footer title="Seguro de Eliminar el Evento:">
                 <div class="d-block text-center">
-                 <h3>{{evento.titulo}}</h3>
+                  <h3>{{evento.titulo}}</h3>
                 </div>
-                <b-button class="mt-3" variant="danger" block @click="eliminarEvento(evento.id_evento)">Eliminar</b-button>
-                <b-button class="mt-3" variant="primary" block @click="hideModal(index)" >Cancelar</b-button>
-               </b-modal>
+                <b-button
+                  class="mt-3"
+                  variant="danger"
+                  block
+                  @click="eliminarEvento(evento.id_evento)"
+                >Eliminar</b-button>
+                <b-button class="mt-3" variant="primary" block @click="hideModal(index)">Cancelar</b-button>
+              </b-modal>
             </td>
             <!-- <td><button class="btn btn-danger" v-on:click="eliminarEvento(evento.id_evento)" >Eliminar</button></td> -->
           </tr>
@@ -247,16 +299,18 @@ export default {
         evt.preventDefault()
         alert('Evento Creado Exitosamente')
         this.enviarFormulario();
+          this.file=""
+          this.form.nombre= ""
+          this.form.descripcion= ""
+          this.form.lugar= ""
+          this.form.fecha= ""
+          this.form.imagen= ""
       },
       onUpdate(evt) {
         evt.preventDefault()
         alert('Evento Actualizado Exitosamente')
         this.actualizarFormulario()
-        this.evento.titulo = ""
-         this.evento.imagen = ""
-        this.evento.descripcion = ""
-        this.evento.lugar = ""
-        this.evento.fecha =""
+        this.show=true;
       },
       cargarDatos(event) {
         this.show = false;
